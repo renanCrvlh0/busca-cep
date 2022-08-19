8ce3d9024667e78679caa4083383083f8ee8150b
Renan Carvalho <91303492+renanDevs@users.noreply.github.com>
2022-08-18 22:59:55 -0300

    JavaScript e CSS do projeto
    Adicionando pastas de JavaScript e CSS para dinamização e estilizando do projeto.

---
+++ js/script.js
@@ -0,0 +1,59 @@
+window.onload = () => {
+    
+    const btn = document.querySelector('.btn');
+    const cep = document.querySelector('#cep');
+    
+    btn.addEventListener('click', async function(){
+        let capturacep = document.querySelector('#cep').value;
+        let url = `https://viacep.com.br/ws/${capturacep}/json/`
+        try{
+            let dados = await fetch(url);
+            let retorno = await dados.json();
+            for(let campo in retorno){
+                if(document.querySelector(`#${campo}`)){
+                    document.querySelector('#buscaCep').classList.add('esconde');
+                    document.querySelector('#container-tabela').classList.add('container-tabela')
+                    document.querySelector('.container-tabela').classList.remove('esconde');
+                    document.querySelector('#textoResultado').classList.remove('esconde');
+                    document.querySelector(`#${campo}`).innerHTML = retorno[campo]; //(`${campo}`[0].toUpperCase() + `${campo}`.substring(1) + ': ') + // retorno[campo];
+                }
+            }
+        } 
+        catch(error){
+            alert('CEP não existe');
+        }
+        
+        if(cep.value.length != 0){
+            const divBtn = document.querySelector('#container-cep');
+            const btnVoltar = document.createElement('button');
+            divBtn.appendChild(btnVoltar);
+            btnVoltar.classList.add('btn2');
+            btnVoltar.textContent = 'VOLTAR';
+        
+            btnVoltar.addEventListener('click', function(){
+                location.reload();
+            })
+        }
+        
+    });   
+    
+    cep.addEventListener('keypress', async function(e){
+        if (e.keyCode == 13){
+            let capturacep = document.querySelector('#cep').value;
+            let url = `https://viacep.com.br/ws/${capturacep}/json/`
+            try{
+                let dados = await fetch(url);
+                let retorno = await dados.json();
+                for(let campo in retorno){
+                    if(document.querySelector(`#${campo}`)){
+                        document.querySelector(`#${campo}`).innerHTML = (`${campo}`[0].toUpperCase() + `${campo}`.substring(1) + ': ') + retorno[campo];
+                    }
+                }
+            } 
+            catch(error){
+                alert('CEP não existe');
+            }
+        }
+    })
+    
+    }
